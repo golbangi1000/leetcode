29b395b4cc99d8f0333cc3856853e3d3e26a54ee
golbangi1000 <kenjames777@naver.com>
2023-11-22 15:53:40 +0900

    wfaew
    awef

--- waef.js
+++ waef.js
@@ -1,40 +1,16 @@
 var twoSum = function (nums, target) {
-   var array1 = [0, 0];
-   var number = 0;
-
+   var array = [0, 0];
    for (i of nums) {
-      var array2 = nums;
-      if (i <= target) {
-         number = target - i;
-         array1[0] = nums.indexOf(i);
-
-         for (y of array2) {
-            if (y == number && nums.indexOf(i) != nums.indexOf(y)) {
-               array1[1] = nums.indexOf(y);
-               console.log("awe");
-
-               if (nums.indexOf(i) == nums.indexOf(y)) {
-                  console.log(nums.indexOf(i) + "     " + nums.indexOf(y));
-                  array[1] = nums.indexOf(y, nums.indexOf(i) + 1);
-               }
-               return array1;
-            }
+      console.log("af");
+      for (let y = nums.indexOf(i) + 1; y < nums.length; y++) {
+         if (i + nums[y] === target) {
+            array[0] = nums.indexOf(i);
+            array[1] = y;
+            return array;
          }
       }
    }
-
-   return array1;
+   return array;
 };
 
-console.log(twoSum([3, 3], 6));
 console.log(twoSum([3, 2, 4], 6));
-// var indices = [];
-// var array = ['a', 'b', 'a', 'c', 'a', 'd'];
-// var element = 'a';
-// var idx = array.indexOf(element);
-// while (idx != -1) {
-//   indices.push(idx);
-//   idx = array.indexOf(element, idx + 1);
-// }
-// console.log(indices);
-// console.log(array);
