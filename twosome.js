e3e77abe234e02d19033d3224be8474d3270deb2
golbangi1000 <kenjames777@naver.com>
2023-11-22 16:01:42 +0900

    aweg
    awge

--- twosome.js
+++ twosome.js
@@ -1,7 +1,6 @@
 var twoSum = function (nums, target) {
    var array = [0, 0];
    for (i of nums) {
-      console.log("af");
       for (let y = nums.indexOf(i) + 1; y < nums.length; y++) {
          if (i + nums[y] === target) {
             array[0] = nums.indexOf(i);
@@ -12,5 +11,5 @@ var twoSum = function (nums, target) {
    }
    return array;
 };
-
+//try hashmap later
 console.log(twoSum([3, 2, 4], 6));
